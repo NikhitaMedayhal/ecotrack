404182f291cba3f69f2f27f9bf8ac4369d6cccf0
NikhitaMedayhal <nikhita.medayhal@yahoo.com>
2026-02-14 11:11:30 +0530

    Add comments to carbon.py functions
    Added comments to clarify the purpose of each function.

--- core/carbon.py
+++ core/carbon.py
@@ -3,11 +3,11 @@ KWH_PER_GB = 0.06          # energy intensity (kWh per GB transferred)
 KG_CO2_PER_KWH = 0.475     # carbon intensity (kg CO2 per kWh)
 
 def bytes_to_gb(b: int) -> float:
-    return b / (1024 ** 3)
+    return b / (1024 ** 3) #converting to gigabytes
 
 def gb_to_energy_kwh(gb: float) -> float:
-    return gb * KWH_PER_GB
+    return gb * KWH_PER_GB #calculating energy consumed
 
 def energy_to_co2_kg(energy_kwh: float) -> float:
-    return energy_kwh * KG_CO2_PER_KWH
+    return energy_kwh * KG_CO2_PER_KWH #calculates the carbon emission
 
